8080a85fee69abd1574158cf300b94d1bde7c511
opei-lab <opei39chari@yahoo.co.jp>
2025-08-19 22:26:15 +0900

    Postモデルを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,52 +1,57 @@
 require 'rails_helper'
- 
+
 describe User do
   let(:nickname) { 'テスト太郎' }
   let(:email) { 'test@example.com' }
-  let(:user) { User.new(nickname: nickname, email: email, password: password, password_confirmation: password) } # 変数に格納
- 
+  let(:password) { '12345678' }
+  let(:user) { User.new(nickname: nickname, email: email, password: password, password_confirmation: password) }
+
   describe '.first' do
     before do
-      create(:user, nickname: nickname, email: email)
+      @user = create(:user, nickname: nickname, email: email) # 修正
+      @post = create(:post, title: 'タイトル', content: '本文', user_id: @user.id) # 修正
     end
- 
+
     subject { described_class.first }
- 
+
     it '事前に作成した通りのUserを返す' do
       expect(subject.nickname).to eq('テスト太郎')
       expect(subject.email).to eq('test@example.com')
     end
+
+    it '紐づくPostの情報を取得できる' do
+      expect(subject.posts.size).to eq(1)
+      expect(subject.posts.first.title).to eq('タイトル')
+      expect(subject.posts.first.content).to eq('本文')
+      expect(subject.posts.first.user_id).to eq(@user.id)
+    end
   end
- 
+
   describe 'validation' do
-    let(:password) { '12345678' }
- 
     describe 'nickname属性' do
       describe '文字数制限の検証' do
         context 'nicknameが20文字以下の場合' do
           let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと' } # 20文字
- 
+
           it 'User オブジェクトは有効である' do
             expect(user.valid?).to be(true)
           end
         end
- 
+
         context 'nicknameが20文字を超える場合' do
           let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' } # 21文字
- 
+
           it 'User オブジェクトは無効である' do
-            user.valid?
- 
             expect(user.valid?).to be(false)
             expect(user.errors[:nickname]).to include('は20文字以下に設定して下さい。')
           end
         end
       end
- 
+
       describe '存在性の検証' do
         context 'nicknameが空欄の場合' do
-          let(:nickname) { '' }
- 
+          let(:nickname) { '' } # 空の文字列
+
           it 'User オブジェクトは無効である' do
             expect(user.valid?).to be(false)
             expect(user.errors[:nickname]).to include("が入力されていません。")
