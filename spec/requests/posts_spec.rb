a8942e82212ba8b2d52fe2a79be4879bcfe804c3
opei-lab <opei39chari@yahoo.co.jp>
2025-09-04 08:45:11 +0900

    学習ログ一覧ページを作成

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   before do
-    @user = create(:user) # 各テストで使用できるユーザーを作成
-    @post = create(:post) # 閲覧用の Post を作成
+    @user = create(:user)
+    @post = create(:post) # 追加
   end
 
   describe 'GET /posts/new' do
@@ -40,10 +40,28 @@ RSpec.describe 'Posts', type: :request do
         expect(response).to have_http_status '200'
       end
     end
+
     context 'ログインしている場合' do
       it 'HTTPステータス200を返す' do
         sign_in @user
         get "/posts/#{@post.id}"
+        expect(response).to have_http_status '200'
+      end
+    end
+  end
+
+  describe 'GET /posts' do
+    context 'ログインしていない場合' do
+      it 'HTTPステータス200を返す' do
+        get '/posts'
+        expect(response).to have_http_status '200'
+      end
+    end
+
+    context 'ログインしている場合' do
+      it 'HTTPステータス200を返す' do
+        sign_in @user
+        get '/posts'
         expect(response).to have_http_status '200'
       end
     end
